7315f4a9d198a2ed9ee2fa788068a7bc47292e77
Ayush Jain <2022bcacsbayush12896@poornima.edu.in>
2023-07-26 10:40:21 -0700

    update

---
+++ lib/practise.dart
@@ -0,0 +1,19 @@
+import 'dart:io';
+void main (){
+
+    print("Welcome to dart!"); 
+    stdout.write("Enter Your name:");
+    var name=stdin.readLineSync();
+    print("Your name is $name");
+    
+       var boy = new Human();
+
+    } 
+
+    class Human{
+      
+      Human();
+
+
+
+    }
