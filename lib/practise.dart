c0d4045dfee78f77cae11366f757cf8505d66249
Ayush Jain <2022bcacsbayush12896@poornima.edu.in>
2023-07-31 10:17:13 -0700

    Update

--- lib/practise.dart
+++ lib/practise.dart
@@ -2,18 +2,35 @@ import 'dart:io';
 void main (){
 
     print("Welcome to dart!"); 
-    stdout.write("Enter Your name:");
-    var name=stdin.readLineSync();
-    print("Your name is $name");
+    // stdout.write("Enter Your name:");
+    // var name=stdin.readLineSync();
+    // print("Your name is $name");
     
-       var boy = new Human();
+      //  var boy = new Human();
 
-    } 
 
-    class Human{
+//declaration of datatypes
+      // int a = 29; 
+      // num percentage=99.65;
+
+      // print(percentage);
+       
+      //  var name = "ayush";
+
+    // } 
+
+    // class Human{
       
-      Human();
+    //   Human();
+        var c = ayush();
+        c.ayu(); 
+       
+       
 
-
-
-    }
+}
+  
+class ayush{
+  void ayu(){
+ print("Ayush");
+}
+}
